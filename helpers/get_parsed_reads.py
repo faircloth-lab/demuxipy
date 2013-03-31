632e2463422f84b3246796943f0a102ad306c035
Brant Faircloth <brant@faircloth-lab.org>
2013-03-31 11:39:23 -0700

    full rewrite of read parsing code

--- helpers/get_parsed_reads.py
+++ helpers/get_parsed_reads.py
@@ -9,145 +9,264 @@ Copyright (c) 2009 Brant Faircloth. All rights reserved.
 USAGE:  python get_parsed_reads.py --configuration=db.conf --all --trimmed
 """
 
-import os, sys, pdb, time, MySQLdb, cPickle, optparse, ConfigParser
-from Bio.SeqIO import QualityIO 
+import os
+import sys
+import time
+import numpy
+import errno
+import argparse
+import ConfigParser
+from collections import defaultdict
+from seqtools.sequence.fasta import FastaQualityReader, FastaWriter
 
-def interface():
-    usage = "usage: %prog [options]"
+import pdb
 
-    p = optparse.OptionParser(usage)
 
-    p.add_option('--configuration', dest = 'conf', action='store', \
-type='string', default = None, help='The path to the configuration file.', \
-metavar='FILE')
-    p.add_option('--species', dest = 'species', action='store', \
-type='string', default = None, help='The species for which to grab sequence')
-    p.add_option('--trimmed', dest = 'trimmed', action='store_true', \
-default = False, help='Get trimmed sequence')
-    p.add_option('--all', dest = 'all', action='store_true',\
-#default = False, help='Get all sequences')
-    p.add_option('--byspecies', dest = 'byspecies', action='store_true', \
-default = False, help='Get sequences by species')
-    p.add_option('--gigantic', dest='gigantic', action='store_true', \
-default = False, help='Store all sequence in one big file')
-    p.add_option('--fasta', dest='fasta', action='store', \
-type='string', default = False, help='Elements you want in the fasta header\
-for each read - separate with a % sign.  Elements include cluster, \
-mid, name, id')
-    (options,arg) = p.parse_args()
-    if not options.conf:
-        p.print_help()
-        sys.exit(2)
-    if not os.path.isfile(options.conf):
-        print "You must provide a valid path to the configuration file."
-        p.print_help()
-        sys.exit(2)
-    return options, arg
+class FullPaths(argparse.Action):
+    """Expand user- and relative-paths"""
+    def __call__(self, parser, namespace, values, option=None):
+        setattr(namespace, self.dest, os.path.abspath(os.path.expanduser(values)))
 
-def not_trimmed(cur, conf, options, sequence, qual):
-    cur.execute('SELECT name FROM sequence WHERE cluster = %s', (options.species))
-    data = cur.fetchall()
-    dataset = set()
-    for d in data:
-        dataset.add(d[0])
-    seqs = QualityIO.PairedFastaQualIterator(
-    open(conf.get('Input','sequence'), "rU"), 
-    open(conf.get('Input','qual'), "rU"))
+
+def is_dir(dirname):
+    if not os.path.isdir(dirname):
+        msg = "{0} is not a directory".format(dirname)
+        raise argparse.ArgumentTypeError(msg)
+    else:
+        return dirname
+
+
+def get_args():
+    """Get arguments from CLI"""
+    parser = argparse.ArgumentParser(
+            description="""Extract demuxipy reads from the database""")
+    parser.add_argument(
+            "fasta",
+            action=FullPaths,
+            help="""The input fasta file"""
+        )
+    parser.add_argument(
+            "quality",
+            action=FullPaths,
+            help="""The input quality file"""
+        )
+    parser.add_argument(
+            "output",
+            action=FullPaths,
+            help="""A directory to hold the output"""
+        )
+    parser.add_argument(
+            "--cluster",
+            type=str,
+            default='all',
+            help="""Grab sequence for a particular cluster"""
+        )
+    parser.add_argument(
+            "--remap",
+            action=FullPaths,
+            type=str,
+            default=None,
+            help="""Given an input Conf file, remap cluster names to something else"""
+        )
+    parser.add_argument(
+            "--remap-section",
+            dest="remap_section",
+            type=str,
+            default=None,
+            help="""Use the specified section within the Conf file"""
+        )
+    parser.add_argument(
+            "--header",
+            choices=['cluster', 'mid', 'name', 'id'],
+            nargs='+',
+            help="""The elements to add to the fasta header""",
+        )
+    parser.add_argument(
+            "--filter-type",
+            dest="filter_type",
+            choices=['regex', 'fuzzy', 'both'],
+            default='regex',
+            help="""Filter the sequences based on match-types""",
+        )
+    parser.add_argument(
+            "--filter-length",
+            dest="filter_length",
+            type=int,
+            default=0,
+            help="""Filter the sequence based on a minimum length"""
+        )
+    args = parser.parse_args()
+    if args.remap:
+        assert args.remap_section, parser.error("If you are remapping with a Conf file, you must pass a Section name.")
+    return args
+
+
+class Header:
+    def __init__(self, h):
+        h = h.replace('>', 'name=')
+        dh = dict((record.split('=') for record in h.split(' ')))
+        for k, v in dh.iteritems():
+            setattr(self, k, v)
+
+
+def mkdir_p(path):
     try:
-        while seqs:
-            record = seqs.next()
-            if record.name in dataset:
-                sequence.write('%s' % record.format('fasta'))
-                qual.write('%s' % record.format('qual'))
-    except StopIteration:
-        pass
-    qual.close()
-    sequence.close()
+        os.makedirs(path)
+    except OSError as exc:
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else:
+            raise
 
-def trimmed(cur, conf, species, sequence, qual):
-    #pdb.set_trace()
-    cur.execute('''SELECT record FROM sequence where cluster = %s''', (species))
-    for p_rec in cur.fetchall():
-        record = cPickle.loads(p_rec[0])
-        if len(record) > 0:
-            sequence.write('%s' % record.format('fasta'))
-            qual.write('%s' % record.format('qual'))
-    qual.close()
-    sequence.close()
+
+def get_remap_dictionary(remap, section):
+    config = ConfigParser.ConfigParser()
+    config.read(remap)
+    return dict(config.items(section))
+
+
+def write_sequences(record, header, output, sample_map, count):
+    if sample_map is not None:
+        header.name = sample_map[header.cluster.lower()]
+    else:
+        header.name = header.cluster
+    record.identifier += ' name={}'.format(header.name)
+    # create the cluster-specific output directory if not exists
+    outdir = os.path.join(output, header.name)
+    mkdir_p(outdir)
+    outf = FastaWriter(
+            os.path.join(outdir, "{}.fasta".format(header.name)),
+            os.path.join(outdir, "{}.qual".format(header.name)),
+            mode='a'
+        )
+    if count != 0 and count % 1000 == 0:
+        sys.stdout.write('.')
+        sys.stdout.flush()
+    outf.write(record)
+    outf.close()
+    count += 1
+    return count, header
+
+
+def filter_match_type(args, header):
+    filtered = True
+    if args.filter_type == 'regex':
+        regex_status = ['regex', 'regex-left', 'regex-right', 'regex-regex-both']
+        if header.outer in regex_status and header.inner in regex_status:
+            filtered = False
+    elif args.filter_type == 'fuzzy':
+        fuzzy_status = ['fuzzy', 'fuzzy-left', 'fuzzy-right', 'fuzzy-fuzzy-both']
+        if header.outer in fuzzy_status and header.inner in fuzzy_status:
+            filtered = False
+    elif args.filter_type == 'both':
+        both_status = [
+            'regex',
+            'regex-left',
+            'regex-right',
+            'regex-regex-both',
+            'fuzzy',
+            'fuzzy-left',
+            'fuzzy-right',
+            'fuzzy-fuzzy-both',
+            'regex-fuzzy-both',
+            'fuzzy-regex-both',
+            ]
+        if header.outer in both_status and header.inner in both_status:
+            filtered = False
+    return filtered
+
+
+def filter_length(args, header):
+    filtered = True
+    if int(header.length) >= args.filter_length:
+        filtered = False
+    else:
+        filtered = True
+    return filtered
+
+
+def get_stats(values):
+    stats = (
+            len(values),
+            numpy.sum(values),
+            numpy.mean(values),
+            numpy.median(values),
+            1.96 * (numpy.std(values, ddof=1) / numpy.sqrt(len(values))),
+            numpy.nanmin(values),
+            numpy.nanmax(values)
+        )
+    return stats
+
+
+def print_stats(stats):
+    print "n:{0[0]:.>18d}\nsum:{0[1]:.>16,.2f}\nmean:{0[2]:.>15.2f}\nmedian:{0[3]:.>13.2f}\n95CI{0[4]:.>16.2f}\nmin:{0[5]:.>16.2f}\nmax:{0[6]:.>16.2f}".format(stats)
+
+
+def get_read_length_summary_stats(summary):
+    print "Read length summary stats"
+    cl = [numpy.mean(v['length']) for k, v in summary.iteritems()]
+    stats = get_stats(cl)
+    print_stats(stats)
+
+
+def get_read_count_summary_stats(summary):
+    print "Read count summary stats"
+    cc = [numpy.sum(v['count']) for k, v in summary.iteritems()]
+    stats = get_stats(cc)
+    print_stats(stats)
+
+
+def get_read_summary_per_sample(summary):
+    print "cluster\tn\tcount\tmean\tmedian\t95CI\tmin\tmax"
+    for k, v in summary.iteritems():
+        #pdb.set_trace()
+        stats = get_stats(v['length'])
+        print "{0}\t{1[0]:d}\t{1[1]:.0f}\t{1[2]:.2f}\t{1[3]:.2f}\t{1[4]:.2f}\t{1[5]:.0f}\t{1[6]:.0f}".format(
+            k,
+            stats,
+            )
+
 
 def main():
-    start_time      = time.time()
-    options, arg    = interface()
+    start_time = time.time()
+    args = get_args()
+    if args.remap:
+        sample_map = get_remap_dictionary(args.remap, args.remap_section)
+    else:
+        sample_map = None
     print 'Started: ', time.strftime("%a %b %d, %Y  %H:%M:%S", time.localtime(start_time))
-    conf            = ConfigParser.ConfigParser()
-    conf.read(options.conf)
-    conn            = MySQLdb.connect(
-                      user=conf.get('Database','USER'),
-                      passwd=conf.get('Database','PASSWORD'),
-                      db=conf.get('Database','DATABASE')
-                      )
-    cur = conn.cursor()
+    try:
+        os.makedirs(args.output)
+    except OSError as exc:
+        if exc.errno == errno.EEXIST:
+            raise OSError("The output directory exists.  Please remove it manually.")
     # get all sequences where cluster != null
     # put them in monolithic file
     # change the sequence header
-    if options.gigantic:
-        outfa       = open('monolithic_PARSED.fsa','w')
-        outqual     = open('monolithic_PARSED.qual','w')
-        if options.trimmed:
-            #pdb.set_trace() 
-            if options.fasta:
-                fs = options.fasta.split('%')
-                fs = ', '.join(fs)
-                query = '''SELECT %s, record FROM sequence WHERE cluster IS NOT NULL and trimmed_len > 0''' % fs
-                results = cur.execute(query)
-            else:
-                results = cur.execute('''SELECT record FROM sequence WHERE cluster IS NOT NULL and trimmed_len > 0''')
-            while 1:
-                result = cur.fetchone()
-                if not result:
-                    break
-                record = cPickle.loads(result[-1])
-                var = len(result) - 1
-                if options.fasta:
-                    record.id = '_'.join([str(i) for i in result[0:var]])
-                    record.name = ''
-                    record.description = ''
-                #pdb.set_trace()
-                outfa.write('%s' % record.format('fasta'))
-                outqual.write('%s' % record.format('qual'))
-
-        outfa.close()
-        outqual.close()
-    
-    elif options.all == 'True' and options.trimmed == 'True':
-        # get all the cluster names from the dbase
-        cur.execute('SELECT distinct(cluster) from sequence')
-        clusters = cur.fetchall()
-        for c in clusters:
-            if c[0] != None:
-                #pdb.set_trace()
-                out = c[0] + '_PARSED.fsa'
-                q = c[0] + '_PARSED.qual'
-                sequence = open(out,'w')
-                qual = open(q,'w')
-                trimmed(cur, conf, c[0], sequence, qual)
-    elif options.trimmed == 'False':
-        out = options.species + '_PARSED.fsa'
-        q = options.species + '_PARSED.qual'
-        sequence = open(out,'w')
-        qual = open(q,'w')
-        not_trimmed(cur, conf, options.species, sequence, qual)
-    elif options.trimmed == 'True':
-        out = options.species + '_PARSED.fsa'
-        q = options.species + '_PARSED.qual'
-        sequence = open(out,'w')
-        qual = open(q,'w')
-        trimmed(cur, conf, options, sequence, qual)
-    cur.close()
-    conn.close()
+    sys.stdout.write("Processing (1 dot / 1000 sequences)")
+    sys.stdout.flush()
+    count = 0
+    summary = defaultdict(lambda: defaultdict(list))
+    if args.cluster == 'all':
+        for record in FastaQualityReader(args.fasta, args.quality):
+            # parse fasta header to get info we need
+            header = Header(record.identifier)
+            match_filter = filter_match_type(args, header)
+            length_filter = filter_length(args, header)
+            if not match_filter and not length_filter:
+                count, header = write_sequences(record, header, args.output, sample_map, count)
+                summary[header.name]['count'].append(1)
+                summary[header.name]['length'].append(int(header.length))
+    print "\n\n"
+    get_read_length_summary_stats(summary)
+    print "\n"
+    get_read_count_summary_stats(summary)
+    print "\n"
+    get_read_summary_per_sample(summary)
     end_time = time.time()
+    print "\n\n"
     print 'Ended: ', time.strftime("%a %b %d, %Y  %H:%M:%S", time.localtime(end_time))
-    print '\nTime for execution: ', (end_time - start_time)/60, 'minutes'
+    print 'Time for execution: ', (end_time - start_time) / 60, 'minutes'
 
 
 if __name__ == '__main__':
